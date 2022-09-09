09090b461a70b82715e04e26a4c3d9a9dfef8b75
Vian Charbonneau <102205118+vianvianc@users.noreply.github.com>
2022-09-08 19:26:39 -0600

    added tables

--- db/schema.sql
+++ db/schema.sql
@@ -13,7 +13,17 @@ CREATE TABLE role (
     title VARCHAR(30),
     salary DECIMAL NOT NULL,
     department_id INT NOT NULL,
-    CONSTRAINT fk_departmentFOREIGN KEY (daprtment_id),
-    REFERENCES movies(id)
+    CONSTRAINT fk_department FOREIGN KEY (daprtment_id),
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT
+    manager_id INT ,
+    CONSTRAINT fk_role FOREIGN KEY (role_id),
+    REFERENCES role(id)
     ON DELETE SET NULL
 );
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO employees (name)
+VALUES ("Jane Doe"),
+        (""),
+        (""),
+        (""),
+        (""),
